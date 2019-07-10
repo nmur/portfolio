49144873b003144bb4f38f2271661bba625c905d
Nicholas Murray <nickmurray91@gmail.com>
2019-07-10 23:51:57 +1000

    moved js to seperate file

---
+++ index.js
@@ -0,0 +1,13 @@
+function onClickAbout() {
+  document.getElementById('details').innerText = 
+  `My name is Nicholas Murray.\n
+  I'm primarily a back-end developer with proficiencies in cloud-based microservices, containers, and C#/.NET.\n
+  I'm passionate about Agile software development, TDD, quality automated testing, and CI/CD.`;
+}
+
+function onClickWork() {
+  document.getElementById('details').innerText = 
+  `2018 - ____: Honeywell - Senior Software Engineer\n
+  2016 - 2018: Honeywell - Software Engineer\n
+  2014 - 2016: Honeywell - Graduate Software Engineer`;
+}
