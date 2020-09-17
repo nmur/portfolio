73ba324b1813183e761c83e0c84776a7d9493ce5
Nicholas Murray <nickmurray91@gmail.com>
2020-09-17 23:19:58 +1000

    updated projects and moved body up

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ function onClickWork() {
 
 function onClickProjects() {
   document.getElementById('details').innerHTML = 
-  `<a href='https://github.com/nmur/portfolio/' ><span class='project-title'  style=\"color:#cb4b16\">portfolio</span></a> - this page<br><br>  
-  <a href='https://github.com/nmur/nsw-license-plate-lookup/' ><span class='project-title' style=\"color:#cb4b16\">nsw-license-plate-lookup</span></a> - simple API for returning vehicle details for NSW plates<br>
+  `<a href='https://nickmurray.dev/poker' ><span class='project-title'  style=\"color:#cb4b16\">planning_poker</span></a> - a simple, no frills Planning Poker web app for team task estimation - used by teams working from home<br><br>
+  <a href='https://github.com/nmur/reddit-video-rotation-bot' ><span class='project-title'  style=\"color:#cb4b16\">reddit-video-rotation-bot</span></a> - a summonable Reddit bot that downloads, rotates, and reuploads videos posted to Reddit<br><br>
+  <a href='https://github.com/nmur/nsw-license-plate-lookup/' ><span class='project-title' style=\"color:#cb4b16\">nsw-license-plate-lookup</span></a> - simple API for returning vehicle details for NSW license plates<br>
   (Try it! <span style=\"color:#268bd2\">GET</span> <a href='https://www.nickmurray.dev/api/plate/ABC123' ><span style=\"color:#859900\">nickmurray.dev/api/plate/ABC123</span></a>)<br><br>  
-  <a href='https://github.com/nmur/flutter_plate_lookup/' ><span class='project-title'  style=\"color:#cb4b16\">flutter_plate_lookup</span></a> - a mobile app for the nsw-license-plate-lookup API, written in Flutter<br><br> 
-  <a href='https://nickmurray.dev/poker' ><span class='project-title'  style=\"color:#cb4b16\">planning_poker</span></a> - a simple, no frills Planning Poker web app for team task estimation`;
+  <a href='https://github.com/nmur/flutter_plate_lookup/' ><span class='project-title'  style=\"color:#cb4b16\">flutter_plate_lookup</span></a> - a mobile app for the nsw-license-plate-lookup API, written in Flutter<br><br>     
+  `;
 }
 
 
